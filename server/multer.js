e4284f904d86c0ec944bc6f568484ed85fd9d7c9
ANIKET SRIVASTAVA <aniket08official@gmail.com>
2025-04-15 16:38:18 +0530

    cloudinary bugs resolved  !

--- server/multer.js
+++ server/multer.js
@@ -3,7 +3,7 @@ import multer from 'multer';
 // Multer setup for file uploads
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
-      cb(null, './assets/');
+      cb(null, './uploads/');
     },
     filename: (req, file, cb) => {
       cb(null, Date.now() + '-' + file.originalname);
